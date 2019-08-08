8d1d37ecb68a7bc4f383843a132c701b169ea138
SourishMurthy1 <murthy.sourish@outlook.com>
2019-08-08 15:44:36 -0700

    created a command based robot and made it a simple Spark tank drive

---
+++ NRGSummerRobot2019/src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,24 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Spark;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+  public static Spark frontLeftSpark = new Spark (0);
+  public static Spark frontRightSpark = new Spark (1);
+  public static Spark rearLeftSpark = new Spark (2);
+  public static Spark rearRightSpark = new Spark (3);
+public static Object backLeft;
+}
